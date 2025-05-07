4aba81f8c9c81690e05100a6052317c45367efe6
Alekhya Nadimpalli <96412033+Github3733@users.noreply.github.com>
2025-05-07 15:43:40 +0530

    Update streamlit_app.py
    pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,11 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 # Query fruit options from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 # Multiselect for ingredients
